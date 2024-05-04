c7479cb8d8dd4bf57456d05fa6c82766bb12083d
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2024-05-04 11:56:08 +0200

    Next UI setup (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <h1>
-      Hello
+    <h1 className="text-3xl font-bold underline bg-red-500">
+      Hello world!
     </h1>
   )
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+// main.tsx or main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {NextUIProvider} from '@nextui-org/react'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </React.StrictMode>,
+  )
+}
