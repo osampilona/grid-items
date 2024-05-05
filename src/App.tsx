b19beea555c007fb18c32f6f30cfaa2736295251
osampilona <brzicamartina@gmail.com>
2024-05-05 14:25:49 +0200

    Smaller fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Tiles from "./components/Tiles";
 const App = () => {
   return (
     <div className="bg-cream min-h-screen w-screen flex flex-col items-center justify-between px-8 py-4">
-      <nav className="flex flex-col lg:flex-row justify-between w-full gap-4">
+      <nav className="flex flex-col lg:flex-row justify-between w-full gap-4 py-8 lg:py-0">
         <h1 className="text-3xl font-bold text-blue w-full flex items-center justify-center lg:justify-start">
           Items Grid
         </h1>
