593280630de5742ce6924acef4ee481f9d1de4a4
osampilona <brzicamartina@gmail.com>
2024-05-08 19:42:39 +0200

    new main.test.tsx

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -1,17 +1,28 @@
-import { render } from "@testing-library/react";
+import { describe, it, expect, beforeAll } from "vitest";
+import App from "./App";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 import { NextUIProvider } from "@nextui-org/react";
-import App from "./App";
 
-describe("App Component", () => {
-  it("renders without crashing", () => {
-    render(
+describe("Root Rendering", () => {
+  let rootElement: HTMLDivElement;
+
+  beforeAll(() => {
+    rootElement = document.createElement("div");
+    rootElement.id = "root";
+    document.body.appendChild(rootElement);
+
+    createRoot(rootElement).render(
       <Provider store={store}>
         <NextUIProvider>
           <App />
         </NextUIProvider>
       </Provider>
     );
+  });
+
+  it("renders without crashing", () => {
+    expect(document.getElementById("root")).not.toBeNull();
   });
 });
